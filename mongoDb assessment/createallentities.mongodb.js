fb3f6f0c2fabdb68a6feb6c7d42583733b2bafd5
olubiyi babajide <olubiyibabajide@gmail.com>
2026-01-03 23:38:01 +0100

    mongodb started

---
+++ mongoDb assessment/createallentities.mongodb.js
@@ -0,0 +1,7 @@
+// entities creation
+db.createCollection("roles");
+db.createCollection("users");
+db.createCollection("categories");
+db.createCollection("products");
+db.createCollection("sizes");
+db.createCollection("orders");
